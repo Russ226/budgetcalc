36d2543f8e90321bfcf39afa7ab37dd915586f1e
russ226 <russ2510@gmail.com>
2018-06-09 22:49:52 -0400

    Working on mapping

---
+++ src/main/java/com/budgetcalc/budgetcalc/BudgetcalcApplication.java
@@ -0,0 +1,12 @@
+package com.budgetcalc.budgetcalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetcalcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BudgetcalcApplication.class, args);
+    }
+}
---
+++ src/main/resources/SQL/DummyData.sql
@@ -0,0 +1,7 @@
+USE budget;
+
+INSERT INTO expenses(amount, expenseName) VALUES
+  (-60.95, "cable bill"),
+  (-75.00, "groceries"),
+  (350.00, "paycheck");
+
