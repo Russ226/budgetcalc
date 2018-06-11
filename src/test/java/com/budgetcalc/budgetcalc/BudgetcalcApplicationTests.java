efda034b91b50f3993f136f39f071f045fc761ce
russ226 <russ2510@gmail.com>
2018-06-10 23:27:03 -0400

    working on setting up the backend

--- src/test/java/com/budgetcalc/budgetcalc/BudgetcalcApplicationTests.java
+++ src/test/java/com/budgetcalc/budgetcalc/BudgetcalcApplicationTests.java
@@ -1,16 +1,37 @@
 package com.budgetcalc.budgetcalc;
 
+import com.budgetcalc.sql.Expense;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ContextConfiguration(locations={"/Context.xml"})
 public class BudgetcalcApplicationTests {
 
+    @Autowired
+    SessionFactory sessionFactory;
+
     @Test
+    @Transactional
     public void contextLoads() {
+        Session session = sessionFactory.getCurrentSession();
+
+        Expense expense = session.get(Expense.class, 1);
+
+        assertEquals(BigDecimal.valueOf(-60.95), expense.getAmount());
+
     }
 
 }
