cf0be7a088d9bd323b78df5e23eea42bfbfe50f0
russ226 <russ2510@gmail.com>
2018-06-13 19:27:15 -0400

    working on routes and html pages

--- src/main/java/com/budgetcalc/budgetcalc/BudgetcalcApplication.java
+++ src/main/java/com/budgetcalc/budgetcalc/BudgetcalcApplication.java
@@ -4,9 +4,11 @@ import com.budgetcalc.controller.BudgetController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@ComponentScan(basePackageClasses = BudgetController.class)
+@ImportResource("classpath:Context.xml")
+@ComponentScan(basePackageClasses = {BudgetController.class})
 public class BudgetcalcApplication {
 
     public static void main(String[] args) {
