43dcb95865e77df5bf29f2066e05efe53478929b
russ226 <russ2510@gmail.com>
2018-06-13 17:15:06 -0400

    set up thyme leaf

--- src/main/java/com/budgetcalc/budgetcalc/BudgetcalcApplication.java
+++ src/main/java/com/budgetcalc/budgetcalc/BudgetcalcApplication.java
@@ -1,9 +1,12 @@
 package com.budgetcalc.budgetcalc;
 
+import com.budgetcalc.controller.BudgetController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackageClasses = BudgetController.class)
 public class BudgetcalcApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/budgetcalc/controller/BudgetController.java
+++ src/main/java/com/budgetcalc/controller/BudgetController.java
@@ -2,14 +2,16 @@ package com.budgetcalc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Controller
-@RequestMapping("/")
 public class BudgetController {
 
     @GetMapping("/current")
     public String getCurrentMonthsBudget(){
+
         return "budgetTable";
     }
+
+
 }
